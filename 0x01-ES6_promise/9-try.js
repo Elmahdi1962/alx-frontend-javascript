abb4886f15a697f43a6e114e2f4e98f725349e11
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-04-05 19:40:48 +0000

    update: fix typo and refactor

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const data = mathFunction();
-    queue.push(data);
-    queue.push('Guardrail was processed');
-    return queue;
+    queue.push(mathFunction(), 'Guardrail was processed');
   } catch (err) {
-    queue.push(err.message);
-    queue.push('Guardrail was processed');
-    return queue;
+    queue.push(`Error: ${err.message}`, 'Guardrail was processed');
   }
+  return queue;
 }
