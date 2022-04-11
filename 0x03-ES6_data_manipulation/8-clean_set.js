9768a01eeac6983f652d01c809bb7074631a8536
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-04-11 18:36:41 +0000

    update3

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
   const len = startString.length;
   const selectedWords = [];
-
   if (startString && typeof (startString) === 'string') {
     for (const v of set) {
-      if (v.startsWith(startString)) {
+      if (v && v.startsWith(startString)) {
         selectedWords.push(v.slice(len));
       }
     }
