d0ff7784b8fea9f691393b9e69b58ca2c4bfc76e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-04-11 18:25:50 +0000

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   const len = startString.length;
   const selectedWords = [];
-  if (startString === '') return '';
 
-  for (const e of set) {
-    if (e.startsWith(startString)) {
-      const t = e.substring(len);
-      selectedWords.push(t);
+  if (startString) {
+    for (const e of set) {
+      if (e.startsWith(startString)) {
+        const t = e.substring(len);
+        selectedWords.push(t);
+      }
     }
   }
   return selectedWords.join('-');
