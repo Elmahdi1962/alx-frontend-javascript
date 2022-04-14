1828a304aa1fe07ccf05ece6ab87f6823a2da4b8
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-04-14 11:31:52 +0000

    Update

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,17 @@
 interface MajorCredits {
     credits: number;
-    brand: string;
+    brand: void;
 }
 
 interface MinorCredits {
     credits: number;
-    brand: string;
+    brand: void;
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): string {
-    return `MajorCredits ${subject1.credits + subject2.credits}`;
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): string {
-    return `MinorCredits ${subject1.credits + subject2.credits}`;
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
